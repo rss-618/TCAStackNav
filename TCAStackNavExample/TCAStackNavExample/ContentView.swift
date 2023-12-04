e71a9e044fd2d2e3cc02372cd7f5fe2ea3897775
Ryan Schildknecht <schildknecht618@gmail.com>
2023-12-03 19:16:22 -0600

    Example App Set-up & Cleanup
    - embedded the example app into the repo that runs off of dev
    - now can effectively test the work i've been doing
    - clean up package text

---
+++ TCAStackNavExample/TCAStackNavExample/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TCAStackNavExample
+//
+//  Created by Ryan Schildknecht on 12/3/23.
+//
+
+import SwiftUI
+import TCAStackNav
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCAStackNavExample/TCAStackNavExample/TCAStackNavExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAStackNavExampleApp.swift
+//  TCAStackNavExample
+//
+//  Created by Ryan Schildknecht on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TCAStackNavExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
