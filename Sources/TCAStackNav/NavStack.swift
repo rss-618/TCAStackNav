a615cb4c3767bdd13fbc561fe3a91666e8a85ce2
Ryan Schildknecht <schildknecht618@gmail.com>
2023-12-05 19:12:51 -0600

    Revert "Stopped Using TCA's thing"
    This reverts commit c17fcd2301e3e2c9fbf94d23379b107b994772ff.

---
+++ Sources/TCAStackNav/NavStack.swift
@@ -0,0 +1,38 @@
+//
+//  NavStack.swift
+//
+//
+//  Created by Ryan Schildknecht on 12/3/23.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+public struct NavStack<
+    Coordinator: CoordinatorProtocol,
+    RootView: View,
+    PotentialView: View
+>: View {
+    
+    let root: RootView
+    let store: StoreOf<Coordinator>
+    var potentialScreens: (Store<Coordinator.Screen.State, Coordinator.Screen.Action>) -> PotentialView
+    
+    public init(store: StoreOf<Coordinator>,
+                @ViewBuilder _ rootView: () -> RootView,
+                @ViewBuilder potentialScreens: @escaping (Store<Coordinator.Screen.State, Coordinator.Screen.Action>) -> PotentialView) {
+        self.store = store
+        self.root = rootView()
+        self.potentialScreens = potentialScreens
+    }
+    
+    public var body: some View {
+        NavigationStackStore(self.store.scope(state: \.stack,
+                                              action: { .stack($0) })
+        ) {
+            root
+        } destination: { store in
+            potentialScreens(store)
+        }
+    }
+}
